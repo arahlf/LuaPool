3db5bf7eed4a3b714045734c4673cd55bf6f4270
Alan Rahlf <arahlf@gmail.com>
2011-10-16 08:29:20 -0500

    Updated rectangle point constrain calculation.

--- rectangle.lua
+++ rectangle.lua
@@ -9,29 +9,44 @@ function Rectangle:initialize(x, y, width, height)
     self.height = height
 end
 
+function Rectangle:setPosition(x, y)
+    self.x = x
+    self.y = y
+end
+
 function Rectangle:contains(x, y)
     return x >= self.x and x <= self.x + self.width and y >= self.y and y <= self.y + self.height
 end
 
 function Rectangle:getConstrainedEndpoint(x, y, angle)
-    assert(self:contains(x, y), "Rectangle does not contain point.")
+    local knownSide
 
-    --[[
-    cases:
-        0
-        90
-        180
-        270
-        every other in between?
-    ]]--
-
-    if (angle == 0) then -- east
-        return self.x + self.width, y
-    elseif (angle == math.pi / 2) then -- south
-        return x, self.y + self.height
-    elseif (angle == math.pi) then -- west
-        return self.x, y
-    elseif (angle == math.pi + math.pi / 2) then -- north
-        return x, self.y
+    if (angle > 0) then
+        knownSide = self.y + self.height - y
+    else
+        knownSide = self.y - y
     end
+
+    local missingSide = knownSide / math.tan(angle)
+
+
+    local xDest = x + missingSide
+    local yDest = angle > 0 and self.y + self.height or self.y
+
+
+    if (x + missingSide > self.x + self.width) then
+        knownSide = self.x + self.width - x
+        missingSide = math.tan(angle) * knownSide
+
+        xDest = self.x + self.width
+        yDest = y + missingSide
+    elseif (x + missingSide < self.x) then
+        knownSide = x - self.x
+        missingSide = math.tan(angle) * knownSide
+
+        xDest = self.x
+        yDest = y - missingSide
+    end
+
+    return xDest, yDest
 end
